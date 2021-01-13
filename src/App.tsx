f327b85a23631f124b55d456a899c60b23cabfab
Oleg Filonchuk <oleh.filonchuk@gmail.com>
2021-01-13 21:34:15 +0200

    add multilanguage support

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const { t } = useTranslation();
   return (
-    <div className="App container mx-auto">
+    <div className="App container p-0">
+      {t('sample')}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import './i18n';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={<div>loading...</div>}>
+      <App />
+    </Suspense>
   </React.StrictMode>,
   document.getElementById('root'),
 );
