4c8ff55845ed5810d3c320acdaca438dbe58c770
Oleg Filonchuk <oifilonchuk@gmail.com>
2021-03-18 23:41:27 +0200

    feat: make tailwind styles work in SB

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,4 @@
-import React from 'react';
-import { addDecorator } from '@storybook/react';
-import Layout from './Layout';
-
-addDecorator(storyFn => <Layout>{storyFn()}</Layout>);
-
+import '../src/index.css';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
