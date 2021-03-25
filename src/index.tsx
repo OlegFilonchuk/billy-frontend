2ff90c5e57e9348feabdc6ea4bfa12a417f23e7c
Oleg Filonchuk <oifilonchuk@gmail.com>
2021-03-25 19:14:58 +0200

    temp: remove jira notifications

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { I18nextProvider } from 'react-i18next';
+import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import i18n from './i18n';
 
 ReactDOM.render(
-  <React.StrictMode>test</React.StrictMode>,
+  <React.StrictMode>
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  </React.StrictMode>,
   document.getElementById('root'),
 );
 
