caa28aaaea8d01be40f025ae70b71f3ea9d2359e
Oleg Filonchuk <oifilonchuk@gmail.com>
2021-03-25 20:10:13 +0200

    temp: leave suspense only

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React, { FC, Suspense } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
-import Layout from './components/Layout/Layout';
-import Categories from './components/pages/Categories';
-import CategoryPage from './components/pages/Categories/CategoryPage';
-import Login from './components/pages/Login';
-import Main from './components/pages/Main';
-import NewOp from './components/pages/NewOp';
-import Signup from './components/pages/Signup';
-import { ROUTES } from './constants';
-import { OpType } from './types';
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// import { QueryClient, QueryClientProvider } from 'react-query';
+// import { ReactQueryDevtools } from 'react-query/devtools';
+// import Layout from './components/Layout/Layout';
+// import Categories from './components/pages/Categories';
+// import CategoryPage from './components/pages/Categories/CategoryPage';
+// import Login from './components/pages/Login';
+// import Main from './components/pages/Main';
+// import NewOp from './components/pages/NewOp';
+// import Signup from './components/pages/Signup';
+// import { ROUTES } from './constants';
+// import { OpType } from './types';
 
 // const queryClient = new QueryClient();
 
