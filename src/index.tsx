391a607a4fd6ad7f2f1ec8f88edcdc1eb52dec68
Oleg Filonchuk <oifilonchuk@gmail.com>
2021-03-25 18:51:12 +0200

    temp: try to build an empty project

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { I18nextProvider } from 'react-i18next';
-import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import i18n from './i18n';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <App />
-    </I18nextProvider>
-  </React.StrictMode>,
+  <React.StrictMode>test</React.StrictMode>,
   document.getElementById('root'),
 );
 
